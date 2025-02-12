8f280adc950a175356fa637b25ef588e1b3bb5cd
heymanshe <himanshiksharma@outlook.com>
2025-02-12 16:16:20 +0530

    single-object working

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,2 @@
 class Customer < ApplicationRecord
-  has_many :orders
-  has_many :reviews
 end
--- db/migrate/20250212103905_create_customers.rb
+++ db/migrate/20250212103905_create_customers.rb
@@ -1,8 +1,9 @@
 class CreateCustomers < ActiveRecord::Migration[8.0]
   def change
     create_table :customers do |t|
-      t.string :name
-      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.integer :store_id
 
       t.timestamps
     end
